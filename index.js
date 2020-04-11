a3d253d5d24d5421083c0e6324cc5e4c6f0efa62
Andre Morais <andre.morais@sinqia.com.br>
2020-04-11 18:07:49 -0300

    Fix CORS errors

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const API_VERSION = '/v1'
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.post(`${API_VERSION}/ping`, (req, res) => {  
 
   const schemaIP = Joi.object({
