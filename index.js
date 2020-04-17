2d643692080b19bfac7c8b01a76c11774c333ac0
André Morais <andre.morais@sinqia.com.br>
2020-04-17 00:57:21 -0300

    Add machine ping support along with isReachable

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const Joi = require('@hapi/joi');
 const express = require('express');
-const http = require('http');
-const isUp = require('is-up');
 const log = require('./util/logger');
 const { port } = require('./config');
+var ping = require('ping');
+const isReachable = require('is-reachable');
 
 const app = express();
 
@@ -21,37 +20,26 @@ app.use(function(req, res, next) {
 
 app.post(`${API_VERSION}/ping`, (req, res) => {  
 
-  const schemaIP = Joi.object({
-    url: Joi.string().ip().required()
-  });
-
-  const schemaURI = Joi.object({
-    url: Joi.string().uri().required()
-  });
-
-  const IPIsValid = schemaIP.valid().validate(req.body).error;
-  const urlIsValid = schemaURI.valid().validate(req.body).error;
-
-  if(IPIsValid && urlIsValid) {
-    if (IPIsValid) {
-      return res.status(400).send(schemaIP.valid().validate(req.body).error.details[0].message);
-    } else if(urlIsValid) {
-      return res.status(400).send(schemaURI.valid().validate(req.body).error.details[0].message);
-    }
-  }
-
   (async () => {
-
-    if (await isUp(req.body.url)) {
-      const ret  = 'host ' + req.body.url + ' is alive';
-      log.info(ret);
-      return res.status(200).send(ret);
-    } else {
-      const ret  = 'host ' + req.body.url + ' is dead';
-      log.error(ret);
-      return res.status(404).send(ret);
-    }
     
+    await isReachable(req.body.url).then(result => {
+      if (result) {
+        log.info('host ' + req.body.url + ' is alive');
+        return res.status(200).send(true);
+        
+      }
+
+      ping.sys.probe(req.body.url, (isAlive) => {
+        if (isAlive) {
+          log.info('host ' + req.body.url + ' is alive');
+          return res.status(200).send(true);
+        }
+        log.error('host ' + req.body.url + ' is dead');
+        return res.status(200).send(false);
+  
+      });
+
+    });
   })();
 
 });
