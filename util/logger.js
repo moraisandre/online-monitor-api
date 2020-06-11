e4bd2134a26682acadcfa601e5aa0bbcc20050ae
André Morais <andre.morais@sinqia.com.br>
2020-06-10 22:58:13 -0300

    Improve logger

--- util/logger.js
+++ util/logger.js
@@ -3,6 +3,17 @@ const { logPath } = require('../config');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, printf } = format;
 
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+var transport = new (winston.transports.DailyRotateFile)({
+  filename: `${logPath}\\online-monitor-%DATE%.log`,
+  datePattern: 'YYYY-MM-DD',
+  zippedArchive: false,
+  maxSize: '4m',
+  maxFiles: '1d'
+});
+
 const myFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${level.toUpperCase()}]: ${message}`;
 });
@@ -18,7 +29,8 @@ const logger = createLogger({
   defaultMeta: { service: 'online-monitor-api' },
   transports: [
     new transports.Console(),
-    new transports.File({ filename: `${logPath}\\online-monitor.log` })
+    // new transports.File({ filename: `${logPath}\\online-monitor.log` }),
+    transport
   ]
 });
 
